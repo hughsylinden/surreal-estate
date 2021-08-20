4370d4d9c275da462fee2dd0ffde856b2bd94e98
hughsylinden <kaarelinden@hotmail.com>
2021-08-21 00:16:38 +0100

    inital commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Surreal Estate</h1>
+    </div>
+  );
+}
+
+export default App;
