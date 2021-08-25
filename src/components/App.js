5e0fc481778089656c55c1df90d4e3af67ada30c
hughsylinden <kaarelinden@hotmail.com>
2021-08-25 19:47:41 +0100

    initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import "../styles/App.css";
 import React from "react";
 
+<style />;
+
 function App() {
   return (
     <div className="App">
       <h1>Surreal Estate</h1>
+      <p>surreal</p>
     </div>
   );
 }
