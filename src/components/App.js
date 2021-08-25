79dcd25b9151906260347f1ced7446cf990be5b6
hughsylinden <kaarelinden@hotmail.com>
2021-08-25 21:01:52 +0100

    added reactrouter functionality and changed navbar components to Link

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import "../styles/App.css";
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1>Surreal Estate</h1>
-      <p>surreal</p>
+      <Switch>
+        <Route exact path="/" component={Properties} />
+        <Route exact path="/add-property" component={AddProperty} />
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/NavBar.css";
+import { Link } from "react-router-dom";
 import logo from "../styles/img/logo.png";
 
 const NavBar = () => {
@@ -7,8 +8,12 @@ const NavBar = () => {
     <div className="navbar">
       <img src={logo} alt="logo" className="navbar-logo" />
       <ul className="navbar-links">
-        <li className="navbar-links-item">view properties</li>
-        <li className="navbar-links-item">add a property</li>
+        <li className="navbar-links-item">
+          <Link to="/">properties</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/add-property">add property</Link>
+        </li>
       </ul>
     </div>
   );
